c88c082cc96cfbe506cb8b2e85e38d4d94d687fb
Hector Nava <hector.r.nava44@gmail.com>
2022-04-02 13:17:35 -0600

    Updated function syntax

--- src/components/HelloDiv.js
+++ src/components/HelloDiv.js
@@ -1,8 +1,8 @@
 // TODO: Fix the return statement so that it renders an `h1` heading tag that introduces the creator
-function HelloDiv() {
-  return "Hello";
+export const HelloDiv = () => {
+  return <h1>Hello</h1>;
 }
 
 // TODO: Fix the export statement
 // Export statements are ES6 syntax that tell Javascript what "thing" we want to make available outside of this file
-export HelloDiv;
+export default HelloDiv;
