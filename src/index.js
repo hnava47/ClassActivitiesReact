9ff156cf07a179d2f90a5aec5afe15998506c75e
Hector Nava <hector.r.nava44@gmail.com>
2022-04-03 07:57:43 -0600

    Updated src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// render takes 2 parameters
+// the 1st one is the component that you want to render
+// the 2nd one is the html element you want to render on
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+  document.getElementById('root')
+);
